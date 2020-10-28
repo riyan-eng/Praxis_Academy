fff82d794afb75975fe1f4a10c12c04392a74da7
riyan <fbriyant11@gmail.com>
2020-10-28 11:45:05 +0700

    28 okt

--- Tugas/kelompok/account/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.loginPage, name="login"),  
-	path('register/', views.registerPage, name="register"),
-	path('logout/', views.logoutUser, name="logout"),
-]
